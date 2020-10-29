dc37121d024fe5d7ab0a4ff7b27623752f272501
Vitaly Ascheulov <vitaly.ascheulov@gmail.com>
2020-10-29 23:35:26 +0700

    initial

---
+++ Task1/Task2/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+
+        private static HttpListener _listener;
+
+        static void Main(string[] args)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:1234/");
+            _listener.Start();
+            _listener.BeginGetContext(OnContext, null);
+
+            Console.ReadLine();
+        }
+
+        private static void OnContext(IAsyncResult ar)
+        {
+            var counter = 1;
+            var ctx = _listener.EndGetContext(ar);
+            _listener.BeginGetContext(OnContext, null);
+
+            Console.WriteLine(DateTime.UtcNow.ToString("HH:mm:ss.fff") + " Handling request");
+
+            var buf = Encoding.ASCII.GetBytes($"<HTML><BODY> Thread {Thread.CurrentThread.ManagedThreadId}, request {counter} </BODY></HTML>");
+            ctx.Response.ContentType = "text/html";
+
+            // simulate work
+            Thread.Sleep(5000);
+
+            ctx.Response.OutputStream.Write(buf, 0, buf.Length); ;
+            ctx.Response.OutputStream.Close();
+
+            Console.WriteLine(DateTime.UtcNow.ToString("HH:mm:ss.fff") + " finished");
+        }
+
+    }
+}
