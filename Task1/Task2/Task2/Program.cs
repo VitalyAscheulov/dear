3ebcd3c23390194bceb211cba89d95743f227158
Vitaly Ascheulov <vitaly.ascheulov@gmail.com>
2020-10-30 10:05:07 +0700

    to async

--- Task1/Task2/Task2/Program.cs
+++ Task1/Task2/Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -9,38 +10,58 @@ namespace Task2
 {
     class Program
     {
-        private static HttpListener _listener;
         [ThreadStatic]
         private static int _counter;
 
+        static bool KeepGoing = true;
+        static List<Task> OngoingTasks = new List<Task>();
+
         static void Main(string[] args)
         {
-            _listener = new HttpListener();
-            _listener.Prefixes.Add("http://localhost:1234/");
-            _listener.Start();
-            _listener.BeginGetContext(OnContext, null);
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:1234/");
+            listener.Start();
+            ProcessAsync(listener).ContinueWith(async task => { await Task.WhenAll(OngoingTasks.ToArray()); });
+
+            var cmd = Console.ReadLine();
+
+            if (cmd.Equals("q", StringComparison.OrdinalIgnoreCase))
+            {
+                KeepGoing = false;
+            }
 
             Console.ReadLine();
         }
 
-        private static void OnContext(IAsyncResult ar)
+        static async Task ProcessAsync(HttpListener listener)
         {
-            var ctx = _listener.EndGetContext(ar);
-            _listener.BeginGetContext(OnContext, null);
-            Console.WriteLine($"{DateTime.UtcNow} Handling request");
 
-            var buf = Encoding.ASCII.GetBytes($"<HTML><BODY> Thread {Thread.CurrentThread.ManagedThreadId}, request {_counter} </BODY></HTML>");
-            ctx.Response.ContentType = "text/html";
-
-            // simulate work
-            Thread.Sleep(10000);
-
-            ctx.Response.OutputStream.Write(buf, 0, buf.Length);
-            ctx.Response.OutputStream.Close();
-
-            Console.WriteLine($"{DateTime.UtcNow} finished");
-            _counter++;
+            while (KeepGoing)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                HandleRequestAsync(context);
+            }
         }
 
+        static async Task HandleRequestAsync(HttpListenerContext context)
+        {
+            await Task.Delay(1000);
+            Perform(context);
+        }
+
+        static void Perform(HttpListenerContext ctx)
+        {
+            HttpListenerResponse response = ctx.Response;
+            string responseString = $"<HTML><BODY> Thread {Thread.CurrentThread.ManagedThreadId}, request {_counter} </BODY></HTML>";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            output.Close();
+            _counter++;
+        }
     }
 }
